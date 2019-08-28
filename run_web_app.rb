e206fc544ef5a865d594bb5d80c8f07e2a9a08b4
Aayush Srivastava <37486345+aayushsrivastava@users.noreply.github.com>
2019-08-28 18:34:39 +0530

    add web server

---
+++ run_web_app.rb
@@ -0,0 +1,26 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require './caesar_cipher.rb'
+
+enable :sessions
+
+get "/" do
+	plaintext  = session.delete(:plaintext)
+	key        = session.delete(:key)
+	ciphertext = session.delete(:ciphertext)
+	erb :form, { :locals => { 
+						:plaintext => plaintext,
+   						:key => key,
+						:ciphertext => ciphertext }	}
+end
+
+post "/" do
+	plaintext  = params["plaintext"]
+	key        = params["key"]
+	ciphertext = caesar_cipher(plaintext, key.to_i)
+
+	session["plaintext"]  = plaintext
+	session["key"]        = key
+	session["ciphertext"] = ciphertext
+	redirect "/"
+end
